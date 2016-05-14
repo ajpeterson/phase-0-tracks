1997b63b57de0eb162c2b0cb477c975141d853fa
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-14 14:52:48 -0700

    Add pseudocode to describe structure of my data structure.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,5 @@
+# Building a data structure of the Premier League
+# The structure will include teams
+#   - within each team will be a nickname
+#   - a hash with name of trophy won and how many times it has been won
+#   - an array of players names
