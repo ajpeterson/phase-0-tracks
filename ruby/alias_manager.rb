2d730fa9ac402e3e377686ba1adde988591ad0d4
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-14 16:27:39 -0700

    Add pseudocode for method to convert a full_name into a spy_name

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,7 @@
+# Take in a full_name as an argument
+# Convert full_name into an array of two strings [first_name, last_name]
+#   - Reverse the order of the array so last_name is listed first and first_name second
+#   - Split the array in an array of individual characters
+#   - Iterate through the array of individual characters changing each vowel into the next vowel in from a, e, i, o, u
+#   - Iterate through the array changing each consonant to the next consonant value in the alphabet
+#   - Join the converted letters back together to form a new spy_name
