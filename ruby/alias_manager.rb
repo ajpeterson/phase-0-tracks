5164564cab8b1d03c52ebee68543cc14fe695e5a
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-15 00:20:38 -0700

    Add new method to satisfy challenge. Original algorithm to difficult.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,16 +6,44 @@
 #   - Iterate through the array changing each consonant to the next consonant value in the alphabet
 #   - Join the converted letters back together to form a new spy_name
 
-#puts "What is your full name (First Last)?"
 full_name = "Andrew Peterson".split('')
-#full_name.class
 
 def cipher(create_spy)
-	create_spy.reverse!
 	create_spy.map! {|letter| letter.next}
-	spy_name = create_spy.join('')
-	p spy_name
+	new_string = create_spy.join('')
+	new_string[6] = ' '
+	new_string.split(' ')
+end
+
+cipher(full_name)
+
+
+
+
+
+=begin
+# Take in a full_name as an argument
+# Convert full_name into an array of two strings [first_name, last_name]
+#   - Reverse the order of the array so last_name is listed first and first_name second
+#   - Split the array into an array of individual characters
+#   - Iterate through the array of individual characters changing each vowel into the next vowel from a, e, i, o, u
+#   - Iterate through the array changing each consonant to the next consonant value in the alphabet
+#   - Join the converted letters back together to form a new spy_name
+
+#puts "What is your full name (First Last)?"
+full_name = "Andrew Peterson".split('')
+
+
+def cipher(create_spy)
+	create_spy.map! {|letter| letter.next}
+	new_string = create_spy.join('')
+	new_string[6] = ' '
+	new_string.split(' ')
+	#new_string.class
+	#new_string.gsub(/[aeiouAEIOU]/){|letter| letter.next}
+
 end
 cipher(full_name)
 #spy_name = cipher(full_name)
 #p spy_name
+=end
