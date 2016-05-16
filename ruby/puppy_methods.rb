8a03e95955eb4d932e2dc36529cced7bec47db74
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-16 13:03:49 -0700

    Add new instance of Puppy and driver code to test instance methods: .fetch() and .roll_over.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,21 @@ class Puppy
     toy
   end
 
+  def roll_over
+    puts "*rolls over*"
+  end
+
 end
 
+# DRIVER Code
+
+# Create new instance of Puppy class
+Cinnamon = Puppy.new
+
+# Test of our fetch instance method
+puts "This is a test of the 'fetch instance method:'"
+Cinnamon.fetch
+
+# Test of our roll_over instance method
+puts "This is a test of our 'roll_over' instance method:"
+Cinnamon.roll_over
