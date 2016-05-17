bc1fe8ff8c8aaa5b21d4e7bb8d33b598d8b1ef8f
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-16 20:08:44 -0700

    Add a second class Cars and two instance methods along with initialize

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -21,10 +21,47 @@ class Puppy
     puts "I liked your face!"
   end
 
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
 end
 
-# DRIVER Code
 
+class Cars
+
+  def initialize
+    puts "Initializing new cars instance..."
+  end
+
+  def color_of_car(color)
+    puts "Your car is #{color}."
+  end
+
+  def travel
+    puts "Enjoy your trip."
+  end
+end
+
+cars_array = []
+index = 0
+
+while index < 50
+  cars_array.push(Cars.new)
+  index += 1
+end
+
+cars_array.each do |car|
+  car.travel
+  car.color_of_car("red")
+end
+
+# DRIVER CODE for Cars class
+jeep = Cars.new
+jeep.color_of_car("blue")
+jeep.travel(500, 200)
+
+
+# DRIVER CODE for Puppy class
 # Create new instance of Puppy class
 Cinnamon = Puppy.new
 
