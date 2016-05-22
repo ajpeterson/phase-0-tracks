b78d6db110bab74ee566fe7c95dbd5a0fb91e915
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-21 17:37:30 -0700

    Add driver code to test the mixin module methods with my classes.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -21,15 +21,30 @@ p Shout.yell_angrily("I love coding")
 
 # RELEASE 3 CODE
 module Shout
-  def say_it_loud(words)
-    words.upcase + "Yeah baby!"
+  def yell_happily(words)
+    words + " Yeah baby! :)"
+  end
+
+  def yell_angrily(words)
+    words.upcase + " Come on!"
   end
 end
 
-class human
+class Human
   include Shout
 end
 
-class lion
+class Lion
   include Shout
 end
+
+# DRIVER CODE TO test
+human = Human.new
+puts "This is testing the Human class with the Shout module:"
+p human.yell_happily("I love coding!")
+p human.yell_angrily("What is this error.")
+
+lion = Lion.new
+puts "This is testing the Lion class with the Shout module:"
+p lion.yell_happily("Let's hunt!")
+p lion.yell_angrily("Whhere are all the gazelles?")
