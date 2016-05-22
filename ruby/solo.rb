7f180331e0dd3872570f49df4f9c6ade4a3113d6
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-21 21:48:07 -0700

    Add pseudocode of class design.

---
+++ ruby/solo.rb
@@ -0,0 +1,19 @@
+# 6.4 SOLO CHALLENGE
+
+=begin
+-----DESIGN FOR CLASS-----
+
+CLASS:
+Soccer Players
+
+CHARACTERISTICS:
+Age: varies
+Position: varies
+Games played: 38
+Years with team: varies
+
+BEHAVIORS :
+Score goals
+Create assists
+Fouls committed
+=end
