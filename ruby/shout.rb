b8111973a3206436cc1fb995a9408c7054e9751c
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-21 17:29:43 -0700

    Add mixin module and two classes to include the module.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,6 @@
+=begin
+# RELEASE 1 CODE
+
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + ":("
@@ -14,3 +17,19 @@ p Shout.yell_angrily("What is your problem")
 
 puts "This is a test of the yell happily method:"
 p Shout.yell_angrily("I love coding")
+=end
+
+# RELEASE 3 CODE
+module Shout
+  def say_it_loud(words)
+    words.upcase + "Yeah baby!"
+  end
+end
+
+class human
+  include Shout
+end
+
+class lion
+  include Shout
+end
