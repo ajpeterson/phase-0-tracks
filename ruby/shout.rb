c0da1b6fa3e462c0f89b377634e7d59c7898ffdb
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-21 17:18:49 -0700

    Add driver code to test modules.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -7,3 +7,10 @@ module Shout
     ":)" + words + ":)"
   end
 end
+
+# DRIVER CODE
+puts "This is a test of the yell angirly method:"
+p Shout.yell_angrily("What is your problem")
+
+puts "This is a test of the yell happily method:"
+p Shout.yell_angrily("I love coding")
