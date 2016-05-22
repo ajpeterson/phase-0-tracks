022823668adc6bd0b945ca515518702c4715b346
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-21 22:03:07 -0700

    Add initialize method to class with 4 attributes.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -9,7 +9,7 @@ Soccer Players
 CHARACTERISTICS:
 Age: varies
 Position: varies
-Games played: 38
+Games played: varies
 Years with team: varies
 
 BEHAVIORS :
@@ -17,3 +17,19 @@ Score goals
 Create assists
 Fouls committed
 =end
+
+class SoccerPlayer
+
+  def initialize(years_with_team, age)
+    @years_with_team = years_with_team
+    @age = age
+    @games_played = 0
+    @position = ["forward", "winger", "midfield", "fullback", "center back", "goalie"]
+  end
+
+end
+
+# DRIVER CODE for Testing
+
+puts "This is a test of our 'initialize' instance method:"
+p player = SoccerPlayer.new(5, 28)
