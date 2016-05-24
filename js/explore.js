4fd852e8630004a78dd00a607248e1bd438af792
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-23 17:43:20 -0700

    Add pseudocode for program to return a string in reverse.

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// Create a program that takes in a string and returns it reversed
+
+// -declare a function that will take a string as a parameter
+// -iterate through that string to return the values in reverse order
+//    -use a loop to iterate over the string backwards
+//    -print the value to the console
