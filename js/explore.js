63c5c760ccfe744420b3238f4f04c77153c55b63
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-24 12:39:10 -0700

    Update pseudocode to reflect new approach to return a phrase in reverse.

--- js/explore.js
+++ js/explore.js
@@ -1,9 +1,15 @@
 // Create a program that takes in a string and returns it reversed
 
 // -declare a function that will take a string as a parameter
-// -iterate through that string to return the values in reverse order
-//    -use a loop to iterate over the string backwards
-//    -print the value to the console
+// -iterate through that string to return the characters in reverse order
+//    -use a loop to iterate over the string of characters in reverse
+//    -print the characters to the console
+//    -store the result in a variable
+// -Declare a function that will take our new variable as an argument
+// -Add the individual character strings to eachother
+//    -use a loop to iterate through the indvidual character strings
+//    -add the strings to together to form one string of the phrase in reverse
+//    -log the result to the console
 
 function reverse(phrase) {
   console.log(phrase);
