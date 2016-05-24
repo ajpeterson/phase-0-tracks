6a9f62cc436fd63c9a502116406e610e4f0cc985
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-23 17:54:10 -0700

    Add function to reverse string and test with driver code.

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,17 @@
 // -iterate through that string to return the values in reverse order
 //    -use a loop to iterate over the string backwards
 //    -print the value to the console
+
+function reverse(str) {
+  var phrase = str;
+
+  for (var i = str.length; i >= 0; i--)   {
+    console.log(str[i]);
+  }
+}
+
+// DRIVER CODE to test functionality (Q3)
+// reverse("hello");
+
+//DRIVER CODE to reverse a string of my choosing
+reverse("Coding is Awesome!");
