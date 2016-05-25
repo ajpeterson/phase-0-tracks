481848e7ff6af459f12319e2471ec92b7313f83b
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-24 22:32:51 -0700

    Add code to push a new name into the name array and a color into the collor array.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,8 @@
 var horseColor = ["metallic gold", "rave green", "crimson", "purple"]
 var horseName = ["Rooney", "Giggs", "Larry", "Grace"]
+
+// Code to add a color to horseColor
+horseColor.push("highlighter pink");
+
+// Code to add a name to horseName
+horseName.push("Bud");
