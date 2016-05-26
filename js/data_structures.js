65f8a0fb21406886deecb63add1a5d3e5c2f9ecd
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-25 19:11:31 -0700

    Add driver code to test arrays are populated correctly.

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,3 +6,7 @@ horseColor.push("highlighter pink");
 
 // Code to add a name to horseName
 horseName.push("Bud");
+
+// Driver Code to test adding to arrays
+// console.log(horseName);
+// console.log(horseColor);
