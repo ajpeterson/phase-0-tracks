5b4c42dce7b27e49c54615ce84854c531a1c839a
Andrew Peterson <ajpeterson79@gmail.com>
2016-06-03 18:08:55 -0700

    Update pseudocode.

--- databases/baby_directory/babies.rb
+++ databases/baby_directory/babies.rb
@@ -2,10 +2,13 @@
 BABY TRACKER:
 Create a class for babies
   - initialize method will create new babies table and set attributes
-    - table will include (id integer primary key, first name, last name, age (in weeks), emergency contact, emergency contact phone number, special needs)
+    - table will include (id integer primary key, first name, last name, age (in weeks), *emergency contact, *emergency contact phone number, special needs)
     - accept first name and last name as arguments
     - attributes = last diaper, last bottle (both attr_accessor)
   - method to add a new baby to the table
+    - input: first name and last name
+    - use INSERT sql call to add a new baby to the table
+    - output: new baby will show as last entry in table
   - method to update a babies age (which set at 0 by default)
     - input: new age in weeks
     - use UPDATE sql call to insert new data into table
@@ -14,9 +17,17 @@ Create a class for babies
     - input: string of special instructions
     - use UPDATE sql call to add or update the special needs of a baby
     - output: new instructions will show under special needs
+
+* will add in if time permits
 =end
 
 class Babies
 
-  
+  def initialize
+  end
+
+  def add_baby
+  end
+
+
 end
