9d13e9a79bddd8d11160f655f2afa4fcb98bffd9
Andrew Peterson <ajpeterson79@gmail.com>
2016-06-03 17:51:40 -0700

    Add pseudocode for babies class.

---
+++ databases/baby_directory/baby_tracker.rb
@@ -0,0 +1,17 @@
+=begin
+BABY TRACKER:
+Create a class for babies
+  - initialize method will create new babies table and set attributes
+    - table will include (id integer primary key, first name, last name, age (in weeks), emergency contact, emergency contact phone number, special needs)
+    - accept first name and last name as arguments
+    - attributes = last diaper, last bottle (both attr_accessor)
+  - method to add a new baby to the table
+  - method to update a babies age (which set at 0 by default)
+    - input: new age in weeks
+    - use UPDATE sql call to insert new data into table
+    - output: new age will show in age column for baby that was updated
+  - method to add a comment for special needs
+    - input: string of special instructions
+    - use UPDATE sql call to add or update the special needs of a baby
+    - output: new instructions will show under special needs 
+=end
