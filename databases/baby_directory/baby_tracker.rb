2ec59c17cf0e3e57b25aee9fc43af4689f079f08
Andrew Peterson <ajpeterson79@gmail.com>
2016-06-06 00:18:01 -0700

    Add add method as start of UI and test.

--- databases/baby_directory/baby_tracker.rb
+++ databases/baby_directory/baby_tracker.rb
@@ -1,6 +1,22 @@
 =begin
 This file contains the interaction for the baby TRACKER
  - require: babies.rb, caregivers.rb, babies_caregivers.rb
+
+ ADD method:
+  - ask if adding baby or caregiver
+  - if baby: collect baby info
+    - first name
+    - last name
+    - age in weeks
+    - special needs
+    - call add baby method passing in collected data
+  - else: collect caregiver info
+    - first name
+    - last name
+    - years experience
+    - cpr certification
+    - first aid certification
+    - call add caregiver method passing in collected data
 =end
 
 # require gems
@@ -17,6 +33,55 @@ db = SQLite3::Database.new("baby_tracker.db")
 baby = Babies.new
 caregiver = Caregivers.new
 join = Junction.new
+
+
+def add(db, baby, caregiver, join)
+  puts "Please specify if you would like to add a 'baby' or 'caregiver' to the directory."
+  user_response = gets.chomp.downcase
+
+  if user_response == "baby"
+    puts "Please provide baby's first name."
+    first_name = gets.chomp
+    puts "Please provide baby's last name."
+    last_name = gets.chomp
+    puts "Please provide baby's age in weeks (please enter a number)."
+    age_in_weeks = gets.chomp.to_i
+    puts "Please list any special needs for the baby."
+    special_needs = gets.chomp
+
+    baby.add_baby(db, first_name, last_name, age_in_weeks, special_needs)
+    puts first_name + " " + last_name + " has been added to the baby directory."
+
+    join.add_for_care(db, "N/A", "N/A", 0, 0)
+
+  else
+    puts "Please provide caregiver's first name."
+    first_name = gets.chomp
+    puts "Please provide caregiver's last name."
+    last_name = gets.chomp
+    puts "Please provide caregiver's years of experience (please enter a number)."
+    yrs_experience = gets.chomp.to_i
+    puts "Is the caregiver CPR certified? (y/n)"
+    cpr_cert = gets.chomp.downcase
+      if cpr_cert == "y"
+        cpr_cert = "true"
+      else
+        cpr_cert = "false"
+      end
+    puts "Is the caregiver First Aid certified? (y/n)"
+    first_aid_cert = gets.chomp.downcase
+      if first_aid_cert == "y"
+        first_aid_cert = "true"
+      else
+        first_aid_cert = "false"
+      end
+
+    caregiver.add_caregiver(db, first_name, last_name, yrs_experience, cpr_cert, first_aid_cert)
+    puts first_name + " " + last_name + " has been added to the caregiver directory."
+  end
+end
+
+add(db, baby, caregiver, join)
 
 =begin
 =================DRIVER CODE for TESTING====================
@@ -43,7 +108,8 @@ baby.special_needs(db, "Allergic to peanuts.", 3)
 caregiver.cpr_update(db, "true", 2)
 caregiver.experience_update(db, 2, 2)
 caregiver.first_aid_update(db, "true", 1)
+caregiver.add_caregiver(db, "Jared", "Peters", 3, "true", "true")
 join.diaper_update(db, "9:15am", 2, 8)
 join.bottle_update(db, "11:05am", 2, 9)
+join.add_for_care(db, "N/A", "N/A", 0, 0)
 =end
-caregiver.add_caregiver(db, "Jared", "Peters", 3, "true", "true")
